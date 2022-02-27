77f72f98d1091c80eea0e74a12f8e1e5673b5bed
cataalboteanu <mihai.alboteanu@mta.ro>
2022-02-27 15:21:50 +0200

    DB creation script

--- routes/index.js
+++ routes/index.js
@@ -14,23 +14,32 @@ const xss_low_controller = require('../controllers/xss_low_controller')
 router.get('/level1Low', validator.validateXSSlowSearch, xss_low_controller.photos_list)
 router.post('/level1Low/update', validator.validateXSSlowPost, xss_low_controller.description_update)
 router.get('/level1Low/warning', function (req, res) {
-  res.render('warning')
+  res.send("Don't try running any scripts")
 })
 
 //XSS hard
 const xss_high_controller = require('../controllers/xss_high_controller')
 router.get('/level1Hard', validator.validateXSShigh, xss_high_controller.photos_list)
 
-//SQLi low
+//noSQLi low
 router.get('/level2Low', function (req, res) {
   res.render('level2Low')
 })
 
+
 //SQLi hard
-const sql_high_controller = require('../controllers/sql_high_controller')
-router.get('/level2Hard', sql_high_controller.product_list)
+const sqli_controller = require('../controllers/sqli_controller')
+router.get('/level2Hard', sqli_controller.product_list)
 
+//noSQLi low
+const nosqli_controller = require('../controllers/nosqli_controller')
+router.post('/level2Low', nosqli_controller.login)
+router.get('/level2Low/user', function (req, res) {
+  res.send('user')
+})
 
+const reset_controller = require('../controllers/db')
+router.get('/reset', reset_controller.reset)
 
 
 
